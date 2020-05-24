9412fee9dd513beb16370369ea7d226d59cc0968
firerick <rixd@qq.com>
2020-05-25 07:02:06 +0800

    Add "index page" which is redirect to login.

--- learning_record/learning_record/urls.py
+++ learning_record/learning_record/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from record.views import index
+
 urlpatterns = [
+    path('', index),
     path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
 ]
