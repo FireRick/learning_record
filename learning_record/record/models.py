a1e3f7d36660fa2d953d8fa92cefa6fda7100c97
firerick <rixd@qq.com>
2020-05-24 16:51:53 +0800

    Modify "models.py".

--- learning_record/record/models.py
+++ learning_record/record/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Item(models.Model):
     """ learning item """
-    id = models.IntegerField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50, verbose_name='Name')
     description = models.CharField(max_length=1000, verbose_name='Description')
     record_sum = models.IntegerField()
@@ -10,8 +10,8 @@ class Item(models.Model):
 
 class Record(models.Model):
     """ learning record """
-    id = models.IntegerField(primary_key=True)
-    time = models.DateTimeField(auto_now_add=True)  # time of learning
+    id = models.AutoField(primary_key=True)
+    time = models.DateTimeField()  # time of learning
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
     # brief intro of what you learned
     key_content = models.CharField(max_length=200, verbose_name='Key Content')
