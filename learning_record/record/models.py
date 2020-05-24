f1ad32f4e2ff2ccb58d57e184da8f302cab74588
firerick <rixd@qq.com>
2020-05-24 16:34:10 +0800

    Add "Item" and "Record" into "models.py" and migrate.

--- learning_record/record/models.py
+++ learning_record/record/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+    """ learning item """
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50, verbose_name='Name')
+    description = models.CharField(max_length=1000, verbose_name='Description')
+    record_sum = models.IntegerField()
+
+
+class Record(models.Model):
+    """ learning record """
+    id = models.IntegerField(primary_key=True)
+    time = models.DateTimeField(auto_now_add=True)  # time of learning
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    # brief intro of what you learned
+    key_content = models.CharField(max_length=200, verbose_name='Key Content')
+    description = models.CharField(max_length=1000, verbose_name='Description')
